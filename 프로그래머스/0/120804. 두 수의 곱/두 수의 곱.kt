fc564e17e62983bc2499e123f36b86d05bc84b31
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2024-05-03 08:02:26 +0900

    [level 0] Title: 두 수의 곱, Time: 0.01 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun limit(num: Int): Boolean {
+        var permission: Boolean = true
+        
+        if(num < 0) permission = false
+        if(num > 100) permission = false
+        
+        return permission
+    }
+    
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        
+        if(limit(num1) && limit(num2))
+            answer = num1 * num2 
+        
+        return answer
+    }
+}
