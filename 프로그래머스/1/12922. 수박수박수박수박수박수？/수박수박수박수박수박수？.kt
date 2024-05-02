2f6f6dd7abb4e3d97d05635b32638dccce798442
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2024-05-03 08:09:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 4.06 ms, Memory: 59.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): String {
+        var answer = ""
+        
+        for(i in 1..n){
+            if(i % 2 != 0) { answer += "수" } else { answer += "박" }
+        }
+        
+        return answer
+    }
+}
