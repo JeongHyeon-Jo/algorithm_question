6cb859a652554f736a92b7f0a1cd5a520dfcf8f1
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2024-05-07 17:24:31 +0900

    [level 1] Title: 시저 암호, Time: 17.36 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -7,9 +7,9 @@ class Solution {
         for((i, v) in sarr.withIndex()){
             if(v.isLetter()){
                 if(v.isUpperCase()){
-                    asci = v.toLowerCase().toInt() + n
-                    if(asci > 122) asci = 97 + (asci - 123)
-                    sarr[i] = asci.toChar().toUpperCase()
+                    asci = v.toInt() + n
+                    if(asci > 90) asci = 65 + (asci - 91)
+                    sarr[i] = asci.toChar()
                 } else {
                     asci = v.toInt() + n
                     if(asci > 122) asci = 97 + (asci - 123)
