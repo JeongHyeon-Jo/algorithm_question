2f34c6dcc7f973709eda1da1cf88d8b838b82d86
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2024-05-07 20:15:37 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 24.53 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(strings: Array<String>, n: Int): Array<String> {
+        var answer = arrayOf<String>()
+        
+        print(strings[1].(1))
+        
+        return answer
+    }
+}
