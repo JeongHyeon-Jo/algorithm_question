8b9c8232968bb5d3f6eb223270bf50fa6b1ae2da
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2025-01-23 16:15:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(price: Int): Int =
+        if(price >= 500_000) {
+            (price * 0.8).toInt()
+        } else if(price >= 300_000) {
+            (price * 0.9).toInt()
+        } else if(price >= 100_000) {
+            (price * 0.95).toInt()
+        } else {
+            price
+        }
+}
