5912e04e7d38107ce678565597b6c7af0db343ce
아블 <167741361+JeongHyeon-Jo@users.noreply.github.com>
2025-02-03 13:21:11 +0900

    [level 0] Title: 모음 제거, Time: 12.16 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(my_string: String): String = my_string
+        .replace("a", "")
+        .replace("e", "")
+        .replace("i", "")
+        .replace("o", "")
+        .replace("u", "")
+}
